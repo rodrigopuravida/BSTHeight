3d775a7934d4af15cab4bf05ec0abe915b7a382e
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-03-29 14:47:03 -0700

    Initial Commit

---
+++ BSTHeight/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BSTHeight
+//
+//  Created by Rodrigo Carballo on 3/29/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
